f20b16cee358e6a22f3da303c7d634db12711197
Lucas <lucas.hoffart2002@gmail.com>
2026-04-22 01:54:09 +0200

    Extract business name and filter sponsored results

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
 
+
+
 def accept_cookie(page):
         cookie_button = page.get_by_role("button", name="Tout accepter")
         if cookie_button.is_visible(timeout=3000):
@@ -15,12 +17,26 @@ def search_target(page):
     page.locator("button[aria-label='Rechercher']").click()
     page.locator("div[role ='article']").first.wait_for(state="visible")
 
+# On recupère les noms seulement si ce n'est pas sponsorisé
+def extract_name(item):
+    lines = item.inner_text().split("\n")
+    lines = [l.strip() for l in lines if l.strip()]
+    if "Sponsorisé" in lines:
+        return None
+    
+    return lines[0]
+
+
 
 with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
+        browser = playwright.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://www.google.com/maps")
         accept_cookie(page)
         search_target(page)
         results = page.locator("div[role='article']")
-        page.pause()
+        for i in range(results.count()):
+            item = results.nth(i)
+            name = extract_name(item)
+            if name:
+                print(name)
